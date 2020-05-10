556ff9cbf5cc44adf79d64e2db0340e0ded3ac16
fikri <fikrihelmi17@gmail.com>
2020-05-10 12:23:07 +0700

    Initial Commit

---
+++ DaftarBuah/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DaftarBuah
+//
+//  Created by Fikri on 10/05/20.
+//  Copyright © 2020 Dicoding Indonesia. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
